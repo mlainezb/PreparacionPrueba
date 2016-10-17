1fd7aaae9b252124c3c35af7ed182626454de3bd
manuel <mlainezb@gmail.com>
2016-10-17 19:56:04 -0300

    Agregar pagina como inicio

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'index/ejercicio'
+  root 'index#ejercicio'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
